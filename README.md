9f84279f747e436dc279741b6a2a6c50f48d7af3
younghoonJ <to.younghoon.jung@gmail.com>
2021-07-18 18:10:17 +0000

    new

---
+++ README.md
@@ -0,0 +1,12 @@
+# new_site
+
+
+npm install tailwindcss @tailwindcss/typography cssnano postcss postcss-import autoprefixer --save-dev
+
+bundle exec jekyll serve
+
+
+JEKYLL_ENV=production NODE_ENV=production bundle exec jekyll build
+
+
+working with tailwindcss / heroicons / alpine js
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/**/*.md',
+      './*.html',
+    ]
+  },
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia,
+        orange: colors.orange,
+        teal: colors.emerald,
+        rose: colors.rose,
+        bluegray: '#141821',
+        fafafa: '#fafafa'
+      },
+      fontFamily: {
+        'noto': ['noto-sans']
+      },
+      width: {
+        '1/7': '14.2857143%',
+        '2/7': '28.5714286%',
+        '3/7': '42.8571429%',
+        '4/7': '57.1428571%',
+        '5/7': '71.4285714%',
+        '6/7': '85.7142857%',
+      }
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
