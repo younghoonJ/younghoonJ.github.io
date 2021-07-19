b0b7bc0bce55d71e6762632e875d38bd419e0673
younghoonJ <to.younghoon.jung@gmail.com>
2021-07-19 00:28:38 +0000

    fix ' to "

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-const colors = require('tailwindcss/colors')
-const plugin = require('tailwindcss/plugin')
+const colors = require("tailwindcss/colors")
+const plugin = require("tailwindcss/plugin")
 
 module.exports = {
   purge: {
     enabled: true,
     content: [
-      './_includes/**/*.html',
-      './_layouts/**/*.html',
-      './_posts/**/*.md',
-      './*.html',
+      "./_includes/**/*.html",
+      "./_layouts/**/*.html",
+      "./_posts/**/*.md",
+      "./*.html",
     ]
   },
   darkMode: false,
@@ -19,24 +19,24 @@ module.exports = {
         orange: colors.orange,
         teal: colors.emerald,
         rose: colors.rose,
-        bluegray: '#141821',
-        fafafa: '#fafafa'
+        bluegray: "#141821",
+        fafafa: "#fafafa"
       },
       fontFamily: {
-        'noto': ['noto-sans']
+        "noto": ["noto-sans"]
       },
       width: {
-        '1/7': '14.2857143%',
-        '2/7': '28.5714286%',
-        '3/7': '42.8571429%',
-        '4/7': '57.1428571%',
-        '5/7': '71.4285714%',
-        '6/7': '85.7142857%',
+        "1/7": "14.2857143%",
+        "2/7": "28.5714286%",
+        "3/7": "42.8571429%",
+        "4/7": "57.1428571%",
+        "5/7": "71.4285714%",
+        "6/7": "85.7142857%",
       }
     },
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
   ],
 }
